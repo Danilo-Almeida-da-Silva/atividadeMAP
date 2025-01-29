c756972e6221aa0baf83cddb5a1577da289bd6c1
Danilo Almeida <danilo.laet52@gmail.com>
2025-01-28 21:44:21 -0300

    adição do arquivo Header

---
+++ src/componets/header/Header.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+export default function Header(){
+    return(
+        <BrowserRouter>
+        <header className={s.header}>
+            <section className={s.Logomarca}>
+                <img src="" alt="" />
+            </section>
+
+        </header>
+        <nav className={s.Navheader}>
+            <ul>
+                <li>
+                    <Link className={s.link} to='/'>Inicio</Link>
+                </li>
+            </ul>
+        </nav>
+        
+        
+        <Routes>
+        <Route path="/" element={<Inicio />} />
+        </Routes>
+        </BrowserRouter>
+
+    )
+
+}
